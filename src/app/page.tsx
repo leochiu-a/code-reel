9eec8d44c463835ac01ec19a7d9e77914d7da268
leochiu <leochiu.chiu@kkday.com>
2025-12-23 16:33:46 +0800

    feat: wrap App component in Suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
-"use client";
-
+import { Suspense } from "react";
 import App from "@/components/App";
 
 export default function Page() {
-  return <App />;
+  return (
+    <Suspense fallback={null}>
+      <App />
+    </Suspense>
+  );
 }
